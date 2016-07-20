79e56153106dedeb5ced55db355b31b8fc9534c9
Scott Scholz <scottscholzpdx@gmail.com>
2016-07-19 20:30:28 -0700

    first revision of backend logic

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+var outputNumbers = [];
+
+
+function pingPong(userNumber) {
+  for (var list=1; list <= list; list +=1) {
+    if(list %15 === 0) {
+      outputNumbers.push("pingpong");
+    } else if (list % 3 === 0) {
+      outputNumbers.push("ping");
+    } else if (list % 5 === 0) {
+      outputNumbers.push("pong");
+    } else {
+      outputNumbers.push("index");
+    }
+  }
+}
